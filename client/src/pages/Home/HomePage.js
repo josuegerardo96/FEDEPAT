5f78cf7e142f36a14c101800777af10922fa6566
josuegerardo96 <josuegerardo09@gmail.com>
2022-10-18 14:22:16 -0600

    Se corrige error admin y se quita parche

--- client/src/pages/Home/HomePage.js
+++ client/src/pages/Home/HomePage.js
@@ -14,7 +14,7 @@ import {GiTeamUpgrade} from "react-icons/gi"
 import {RiArrowRightSLine, RiCalendarTodoFill, RiAdminLine, RiMailDownloadFill, RiListOrdered, RiFilter3Line, RiTeamFill} from "react-icons/ri"
 
 
-import { useSignupAdmin } from "../../hooks/useSignUpAdmin";
+
 
 export default function HomePage(){
     const { user } = useAuth()
@@ -22,26 +22,14 @@ export default function HomePage(){
     // These are the consts in the "LoginPage.js" file
     const [email,setEmail] = useState('')
     const [password, setPassword] = useState ('')
-    // const { login , error, isLoading } = useLogin()
+    const { login , error, isLoading } = useLogin()
 
 
-    // // Another function from the "LoginPage.js" file
-    // const handleSubmit = async(e) =>{
-    //     e.preventDefault()
-    //     await login(email,password)
-    // }
-
-
-    const {signup, error, isLoading } = useSignupAdmin()
-
+    // Another function from the "LoginPage.js" file
     const handleSubmit = async(e) =>{
         e.preventDefault()
-        await signup('fedepat123@gmail.com', 'Fedepat123.',roles.admin,'fedepat','FEDEPAT','11223344',true)
+        await login(email,password)
     }
-
-
-
-
 
 
     // Here starts the design
